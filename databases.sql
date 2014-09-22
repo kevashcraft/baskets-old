ef179870146295dda09e9f32ffa5d0a1d8ec5530
Kevin Ashcraft <github@kevashcraft.com>
2014-09-22 14:05:53 -0400

    added more tables

--- databases.sql
+++ databases.sql
@@ -10,10 +10,10 @@ DROP DATABASE IF EXISTS baskets;
 -- tables:
 --	baskets
 --	contractors
+--	helpers
+--	images
 --	jobs
 --	jobparts
---	jobworkers
---	images
 --	models
 --	parts
 --	suppliers
@@ -24,6 +24,48 @@ DROP DATABASE IF EXISTS baskets;
 
 CREATE DATABASE baskets;
 GRANT ALL ON baskets.* TO baskets@localhost IDENTIFIED BY 'baskets';
+CREATE TABLE baskets(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	dt DATETIME,
+	dtu DATETIME,
+	basket VARCHAR(16),
+	PRIMARY KEY (id)
+) ENGINE=innoDB;
+
+CREATE TABLE contractors(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	dt DATETIME,
+	dtu DATETIME,
+	contractor VARCHAR(128),
+	PRIMARY KEY (id)
+) ENGINE=innoDB;
+
+CREATE TABLE helpers(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	dt DATETIME,
+	dtu DATETIME,
+	name VARCHAR(128),
+	email VARCHAR(128),
+	phone VARCHAR(32),
+	address VARCHAR(256),
+	firstday DATE,
+	lastday DATE,
+	employed BOOLEAN,
+	PRIMARY KEY (id)
+) ENGINE=innoDB;
+
+CREATE TABLE images(
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	dt DATETIME,
+	filename VARCHAR(32),
+	partid INT UNSIGNED,
+	workerid INT UNSIGNED,
+	helperid INT UNSIGNED,
+	userid INT UNSIGNED,
+	jobid INT UNSIGNED,
+	PRIMARY KEY (id)
+) ENGINE=innoDB;
+
 CREATE TABLE jobs(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	dt DATETIME,
@@ -31,7 +73,15 @@ CREATE TABLE jobs(
 	title VARCHAR(128),
 	contractor INT UNSIGNED,
 	model INT UNSIGNED,
+	worker INT UNSIGNED,
+	helper INT UNSIGNED,
 	PRIMARY KEY (id)
+) ENGINE=innoDB;
+
+CREATE TABLE jobparts(
+	jid INT UNSIGNED,
+	pid INT UNSIGNED,
+	room VARCHAR(32),
 ) ENGINE=innoDB;
 
 CREATE TABLE parts(
