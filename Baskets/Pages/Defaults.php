9059e6d6c844f78e315888b2c505ad9d322feea7
Kevin Ashcraft <github@kevashcraft.com>
2014-09-28 18:49:44 -0400

    added menu framework

--- Baskets/Pages/Defaults.php
+++ Baskets/Pages/Defaults.php
@@ -10,6 +10,7 @@ class Defaults
 	<head>
 	<link rel='stylesheet' type='text/css' href='<?php echo MY_URL ?>/reset.css' />
 	<link rel='stylesheet' type='text/css' href='<?php echo MY_URL ?>/style.css' />
+	<link rel='icon' type='image/png' href='<?php echo MY_URL ?>/favicon.png'>
 	<script src='https://code.jquery.com/jquery-2.1.1.min.js'></script>
 	<meta name="viewport" content="width=device-width, user-scalable=no">
 <?php
@@ -22,6 +23,41 @@ class Defaults
 	<div class='page-header'>
 		<img src='<?php echo MY_URL?>/img/logo.png'>
 		<img src='<?php echo MY_URL?>/img/text.png'>
+	</div>
+	<div class='page-nav-container'>
+		<div class='nav-img'><img src='<?php echo MY_URL?>/img/nav-basket.jpg'></div>
+		<nav>
+			<li>Parts
+				<li><a href='<?php echo MY_URL?>/parts/list'>List</a></li>
+				<li><a href='<?php echo MY_URL?>/parts/suppliers'>Suppliers</a></li>
+				<li><a href='<?php echo MY_URL?>/parts/bids'>Bids</a></li>
+			</li>
+			<li>Estimates
+				<li><a href='<?php echo MY_URL?>/estimates/new'>New</a></li>
+				<li><a href='<?php echo MY_URL?>/estimates/active'>Active</a></li>
+				<li><a href='<?php echo MY_URL?>/estimates/all'>All</a></li>
+			</li>
+			<li>Warehouse
+				<li><a href='<?php echo MY_URL?>/warehouse/check-in'>Check-in</a></li>
+				<li><a href='<?php echo MY_URL?>/warehouse/orders'>Orders</a></li>
+				<li><a href='<?php echo MY_URL?>/warehouse/report'>Report</a></li>
+			</li>
+			<li>Scheduling
+				<li><a href='<?php echo MY_URL?>/scheduling/today'>Today</a></li>
+				<li><a href='<?php echo MY_URL?>/scheduling/tomorrow'>Tomorrow</a></li>
+				<li><a href='<?php echo MY_URL?>/scheduling/pending'>Pending</a></li>
+			</li>
+			<li>Jobs
+				<li><a href='<?php echo MY_URL?>/jobs/new'>New</a></li>
+				<li><a href='<?php echo MY_URL?>/jobs/current'>Current</a></li>
+				<li><a href='<?php echo MY_URL?>/jobs/all'>All</a></li>
+			</li>
+			<li>Parts
+				<li><a href='<?php echo MY_URL?>/workers/hire'>Hire</a></li>
+				<li><a href='<?php echo MY_URL?>/workers/all'>All</a></li>
+				<li><a href='<?php echo MY_URL?>/workers/search'>Search</a></li>
+			</li>
+		</nav>
 	</div>
 <?php
 
