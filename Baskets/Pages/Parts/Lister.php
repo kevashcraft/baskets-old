1143f915a7504861b557611e8aede79682adfb7f
Kevin Ashcraft <github@kevashcraft.com>
2014-10-29 02:43:53 -0400

    added pager to proposals and standardized

--- Baskets/Pages/Parts/Lister.php
+++ Baskets/Pages/Parts/Lister.php
@@ -6,14 +6,13 @@ class Lister
 	public static function lister()
 	{
 		\Baskets\Pages\Framework::page_header('Parts List | Baskets');
-		$partpage = isset(\Baskets\Tools\Tracker::$uri[3]) ? intval(\Baskets\Tools\Tracker::$uri[3]) : '0';
-		$partpage = is_int($partpage) ? $partpage : '0';
-		$partlimit = $partpage * 50;
-		$partsstm = \Baskets::$db->prepare("SELECT * FROM parts ORDER BY partid ASC LIMIT $partlimit,50");
-		$pricestm = \Baskets::$db->prepare("SELECT price FROM bidparts WHERE partid=? ORDER BY price ASC");
-		$partsstm->execute();
-		$totalparts = \Baskets::$db->query("SELECT COUNT(*) FROM parts")->fetchColumn();
-		$numopartpages = $totalparts / 50;
+		$page = isset(\Baskets\Tools\Tracker::$uri[3]) ? intval(\Baskets\Tools\Tracker::$uri[3]) : '0';
+		$page = is_int($page) ? $page : '0';
+		$limit = $page * 50;
+		$stm = \Baskets::$db->prepare("SELECT * FROM parts ORDER BY partid ASC LIMIT $limit,50");
+		$stm->execute();
+		$totalrows = \Baskets::$db->query("SELECT COUNT(*) FROM parts")->fetchColumn();
+		$numopages = $totalrows / 50;
 ?>
 <div class='main-viewer' id='main-viewer'>
 	<div class='dash-box'>
@@ -22,20 +21,20 @@ class Lister
 			<a href='<?=MY_URL?>/parts/add' class='add-button'>Add Parts</a>
 		</div>
 		<div class='page_list'>
-			<h2>Total Parts: <?=$totalparts?></h2>
+			<h2>Total Parts: <?=$totalrows?></h2>
 			<a href='<?=MY_URL?>/parts/list'>1</a>...
 			<?
 				for($x=-1;$x<=1;$x++) {
-					$prev = $partpage + $x + 1;
-					if($prev <= 0 || $prev > $numopartpages) continue;
-					$url = MY_URL . '/parts/list/' . $prev;
-					echo "<a href='$url'>$prev</a> ";
+					$nexprev = $page + $x + 1;
+					if($nexprev <= 1 || $nexprev > $numopages) continue;
+					$url = MY_URL . '/parts/list/' . $nexprev;
+					echo "<a href='$url'>$nexprev</a> ";
 				}
 			?>
 		</div>
 		<ul class='part-list list'>
 <?
-	while($parts = $partsstm->fetch()) {
+	while($parts = $stm->fetch()) {
 ?>
 			<li>
 				<span class='part-id'><?=$parts['id']?></span>
--- Baskets/Pages/Proposals/Lister.php
+++ Baskets/Pages/Proposals/Lister.php
@@ -4,16 +4,14 @@ class Lister
 {
 	public static function lister()
 	{
+		$page = isset(\Baskets\Tools\Tracker::$uri[3]) ? intval(\Baskets\Tools\Tracker::$uri[3]) : '0';
+		$page = is_int($page) ? $page : '0';
+		$limit = $page * 50;
+		$stm = \Baskets::$db->prepare("SELECT proposals.*,contractors.contractor FROM proposals,contractors WHERE contractors.id=proposals.contractorid ORDER BY proposals.id ASC LIMIT $limit,50");
+		$stm->execute();
+		$totalrows = \Baskets::$db->query("SELECT COUNT(*) FROM proposals")->fetchColumn();
+		$numopages = $totalrows / 50;
 
-		$proppage = isset(\Baskets\Tools\Tracker::$uri[3]) ? \Baskets\Tools\Tracker::$uri[3] : '0';
-		$propstartcount = $proppage * 50;
-
-		$propstm = \Baskets::$db->prepare("SELECT proposals.*,contractors.contractor FROM proposals,contractors WHERE contractors.id=proposals.contractorid AND proposals.id>=? ORDER BY proposals.id ASC LIMIT 50");
-		$propstm->execute(array($propstartcount));
-
-		$totalprops = \Baskets::$db->query("SELECT COUNT(*) FROM proposals")->fetchColumn();
-
-		$numofpages = $totalprops / 50;
 
 		\Baskets\Pages\Framework::page_header('Proposals List | Baskets');
 ?>
@@ -23,25 +21,35 @@ class Lister
 			<h1><i class='fa fa-leaf'></i> All Proposals</h1>
 			<a href='<?=MY_URL?>/proposals/new' class='add-button'>Add Proposal</a>
 		</div>
-	</div>
-	<ul class='list prop-list'>
-
+		<div class='page_list'>
+			<h2>Total Proposals: <?=$totalrows?></h2>
+			<a href='<?=MY_URL?>/proposals/list'>1</a>...
+			<?
+				for($x=-1;$x<=1;$x++) {
+					$nexprev = $page + $x + 1;
+					if($nexprev <= 1 || $nexprev > $numopages) continue;
+					$url = MY_URL . '/proposals/list/' . $nexprev;
+					echo "<a href='$url'>$nexprev</a> ";
+				}
+			?>
+		</div>
+		<ul class='list prop-list'>
 
 <?php
-	while($prop = $propstm->fetch())
+	while($prop = $stm->fetch())
 	{
 ?>
-		<li>
-			<span class='prop-id'><?=$prop['id']?></span>
-			<span class='prop-contractor'><?=$prop['contractor']?></span>
-			<span class='prop-model'><?=$prop['model']?></span>
-		</li>
+			<li>
+				<span class='prop-id'><?=$prop['id']?></span>
+				<span class='prop-contractor'><?=$prop['contractor']?></span>
+				<span class='prop-model'><?=$prop['model']?></span>
+			</li>
 <?
 	}
 ?>
 
-
-	</ul>
+		</ul>
+	</div>
 </div>
 <?
 		\Baskets\Pages\Framework::page_footer();
